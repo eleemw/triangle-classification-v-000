f36aff512b4b13630319ca5b1508256e39f1284c
Erica Murdock-Waters <ericahpa@gmail.com>
2020-03-09 22:37:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
   end
 
   def kind
-    self.sides
+    #self.sides
       if (@sides[0] == @sides[1]) && (@sides[1] == @sides[2])
         :equilateral
       elsif (@sides[0] == @sides[1]) || (@sides[1] == @sides[2])
